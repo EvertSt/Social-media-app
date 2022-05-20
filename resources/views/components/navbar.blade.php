e902c0b3d47af2d9aee074ae29092e7f6738cb1a
Evert Stijnen <evertstijnen@outlook.com>
2022-05-20 16:12:46 +0200

    initialize git

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+<nav class="container">
+    <div class="row">
+        <div class="col-sm">
+            <p>logo</p>
+        </div>
+        <div class="col-sm">
+            <p>zoekbalk</p>
+        </div>
+        <div class="col-sm">
+            <p>Account</p>
+        </div>
+    </div>
+</nav>
